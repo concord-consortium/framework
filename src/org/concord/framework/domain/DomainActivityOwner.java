5c656b2570187e57bc037ef3cd5650e35af560a8
eburke <eburke@6e01202a-0783-4428-890a-84243c50cc2b>
2004-05-02 12:33:26 +0000

    Fixed updated references
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/core/framework@934 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/framework/domain/DomainActivityOwner.java
+++ src/org/concord/framework/domain/DomainActivityOwner.java
@@ -1,21 +1,20 @@
 package org.concord.framework.domain;
 
+import java.util.Vector;
 import org.concord.framework.logging.LogManager;
 
 public interface DomainActivityOwner
 {
-    String getActivityName();
-    String getActivityTagName();
-    java.util.Vector getDomainUsers();
-    DomainProxyInfo getProxyInfo();
-    long getActivityCreationTime();
-    DomainVersionInfo  getDomainVersionInfo();
-    int getActivityInstanceID();
-    int getActivityID();
-    int getTopicID();
-    LogManager getLogManager();
-    
-    
+    public String getActivityName();
+    public String getActivityTagName();
+    public Vector getDomainUsers();
+    public DomainProxyInfo getProxyInfo();
+    public long getActivityCreationTime();
+    public DomainVersionInfo  getDomainVersionInfo();
+    public int getActivityInstanceID();
+    public int getActivityID();
+    public int getTopicID();
+    public LogManager getLogManager();
     void closeEverything();
 }
 
