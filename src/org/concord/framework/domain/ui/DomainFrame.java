acd90f4371d6d7325cbaea99fc50ecc6d5ce31b8
eburke <eburke@6e01202a-0783-4428-890a-84243c50cc2b>
2004-05-10 19:05:13 +0000

    *** empty log message ***
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/core/framework@1012 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/framework/domain/ui/DomainFrame.java
+++ src/org/concord/framework/domain/ui/DomainFrame.java
@@ -1,207 +1,30 @@
+/*
+ * Created on May 10, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
 package org.concord.framework.domain.ui;
 
+/**
+ * @author eburke
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
 
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.Reader;
-import java.io.Writer;
-import java.util.Vector;
 
 import javax.swing.JFrame;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-import org.concord.framework.closehelper.ApplCloseHandler;
-import org.concord.framework.closehelper.ApplCloseHandlerFactory;
-import org.concord.framework.closehelper.ApplCloseHandlerListener;
-import org.concord.framework.domain.DomainActivityOwner;
-import org.concord.framework.domain.DomainEngine;
-import org.concord.framework.domain.DomainOpener;
-import org.concord.framework.domain.DomainView;
-import org.concord.framework.logging.LogManager;
 
 
 public class DomainFrame 
 extends JFrame
-implements ApplCloseHandlerListener, DomainView
 {
-	protected DomainActivityOwner domainActivityOwner;
-	protected DomainOpener domainOpener;
-	protected LogManager logManager;
-	protected Object activity;
 
 	public DomainFrame()
 	{
 		super();
-        setIconImage(org.concord.framework.domain.DomainUtil.FRAME_ICON.getImage());
-
+ 
 	}
-	
-	public DomainFrame(String title)
-	{
-		super(title);
-        setIconImage(org.concord.framework.domain.DomainUtil.FRAME_ICON.getImage());
-
-	}
-	
-	protected WindowListener windowListener = new WindowAdapter()
-	{
-		public void windowClosing(WindowEvent event)
-		{
-			if (exitOnClose)
-			{
-				closeEverything();
-				dispose();
-        		org.concord.framework.domain.DomainUtil.systemExit();
-			}
-			else
-			{
-				closeEverything();
-				setVisible(false);
-				dispose();
-			}
-		}
-	    public void windowOpened(WindowEvent event)
-	    {
-//	        if(RunActivity.pacifier != null) RunActivity.pacifier.stop();
-	        if(domainOpener != null) domainOpener.closeEverything();
-	    }
-	};
-	
-	protected boolean exitOnClose = defaultExitOnClose();
-	
-	protected boolean defaultExitOnClose()
-	{
-		addWindowListener(windowListener);
-		return !org.concord.framework.domain.DomainUtil.isSingleVM();
-	}
-	
-	public void setExitOnClose(boolean value)
-	{
-		exitOnClose = value;
-	}
-	
-	public boolean isExitOnClose()
-	{
-		return exitOnClose;
-	}
-	
-	public boolean isFocusTraversable()
-	{
-		return true;
-	}
-	
-	public void addNotify()
-	{
-		super.addNotify();
-		ApplCloseHandler appHandler = ApplCloseHandlerFactory.getApplCloseHandler();
-		if(appHandler != null)
-		{
-			appHandler.registerHandlers(this);
-		}
-		
-	}
-	
-	public LogManager getLogManager(){return logManager;}
-	public void setLogManager(LogManager logManager){this.logManager = logManager;}
-	
-	public DomainOpener getDomainOpener(){return domainOpener;}
-	public void setDomainOpener(DomainOpener domainOpener){this.domainOpener = domainOpener;}
-	
-	public void closeEverything()
-	{
-		if(domainActivityOwner != null)
-		{
-			domainActivityOwner.closeEverything();
-		}
-		if(logManager != null) logManager.close();
-		removeWindowListener(windowListener);
-	}
-
-	public void setActivity(Object activity)
-	{
-		this.activity = activity;
-	}
-
-	public void handleQuit()
-	{
-		closeEverything();
-        org.concord.framework.domain.DomainUtil.systemExit();
-		if(org.concord.framework.domain.DomainUtil.isSingleVM())
-		{
-			setVisible(false);
-		}
-	}
-	
-	
-	public void closeActivityFrame()
-	{
-		closeEverything();
-        org.concord.framework.domain.DomainUtil.systemExit();
-		if(org.concord.framework.domain.DomainUtil.isSingleVM())
-		{
-			setVisible(false);
-		}
-	}
-	
-	public DomainView create(String viewSpecification)
-	{
-		return this;
-	}
-	public void release(DomainView view){}
-	
-	public Vector getViewMethods()
-	{
-		return null;
-	}
-	
-	public Vector getViewEvents()
-	{
-		return null;
-	}
-	
-	public Vector getViewActions()
-	{
-		return null;
-	}
-	
-	public DomainEngine getEngine()
-	{
-		return null;
-	}
-	
-	public void setDomainActivityOwner(Object domainActivityOwner)
-	{
-		if(domainActivityOwner instanceof DomainActivityOwner)
-		{
-			this.domainActivityOwner = (DomainActivityOwner)domainActivityOwner;
-		}
-	}
-	
-	public void save(Writer writer)
-	{
-	}
-	
-	public void restore(Reader reader)
-	{
-	}
-	
-	public void save(OutputStream output)
-	{
-	}
-	
-	public void restore(InputStream input)
-	{
-	}
-	
-	public Element serializeAsElement(Document document)
-	{
-		return null;
-	}
-
 }
-
