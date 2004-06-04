e35a50926d772494c8e4c4dc7aae623ec0359fce
eburke <eburke@6e01202a-0783-4428-890a-84243c50cc2b>
2004-06-04 04:23:22 +0000

    Fixed some class loader issues
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/core/framework@1077 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/framework/domain/Version.java
@@ -0,0 +1,33 @@
+package org.concord.framework.domain;
+
+import java.util.Date;
+
+import org.concord.domain.DomainProperties;
+
+public class Version
+extends DomainVersionInfo
+{
+	
+	public void save(DomainProperties properties)
+	{
+		properties.setProperty("major.release", "" + majorRelease);
+		properties.setProperty("minor.release", "" + minorRelease);
+		properties.setProperty("major.version", "" + majorVersion);
+		properties.setProperty("minor.version", "" + minorVersion);
+		properties.setProperty("build", "" + build);
+		properties.setProperty("build.os", buildOS);
+		properties.setProperty("build.java", buildJava);
+		properties.setProperty("build.date", (new Date()).toString());
+		properties.setProperty("developer", developer);
+	}
+	
+	public void restore(DomainProperties properties)
+	{
+		majorRelease = Integer.parseInt(properties.getProperty("major.release"));
+		minorRelease = Integer.parseInt(properties.getProperty("minor.release"));
+		majorVersion = Integer.parseInt(properties.getProperty("major.version"));
+		minorVersion = Integer.parseInt(properties.getProperty("minor.version"));
+		build = Integer.parseInt(properties.getProperty("build"));
+	}
+}
+
