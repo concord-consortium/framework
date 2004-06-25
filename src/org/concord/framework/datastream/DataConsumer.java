07fda3ca84fca334e39e43fd7d439a50f330ce24
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2004-06-25 16:48:47 +0000

    moving data stream to inside of data framwork
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/core/framework@1141 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/framework/datastream/DataConsumer.java
+++
@@ -1,8 +0,0 @@
-package org.concord.framework.datastream;
-
-public interface DataConsumer 
-{
-	public void addDataSource(DataProducer source);
-
-	public void removeDataSource(DataProducer source);
-}
