e8b1458d47d1a6d6b543919a41297e30660c2a2e
imoncada <imoncada@6e01202a-0783-4428-890a-84243c50cc2b>
2004-08-07 22:04:10 +0000

    Modified method names for data producer
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/core/framework@1360 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/framework/data/stream/DataConsumer.java
+++ src/org/concord/framework/data/stream/DataConsumer.java
@@ -2,7 +2,7 @@ package org.concord.framework.data.stream;
 
 public interface DataConsumer 
 {
-	public void addDataSource(DataProducer source);
+	public void addDataProducer(DataProducer source);
 
-	public void removeDataSource(DataProducer source);
+	public void removeDataProducer(DataProducer source);
 }
