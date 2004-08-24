0a8feb83b02c4bd8a5f8572f4f8bcd7f33d87a49
dima <dima@6e01202a-0783-4428-890a-84243c50cc2b>
2004-08-24 13:48:01 +0000

    checking for gl4java was added
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/core/framework@1418 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/framework/system/SystemResourcesChecker.java
+++ src/org/concord/framework/system/SystemResourcesChecker.java
@@ -4,13 +4,23 @@ package org.concord.framework.system;
  * @author dima
  *
  */
+
+import java.lang.reflect.*;
+
+
 public final class SystemResourcesChecker
 {
+	
+	public static final String QUICKTIME_NAME  = "quicktime";
+	public static final String GL4JAVA_NAME     = "gl4java";
+
 	public static boolean checkResource(String resName){
 		boolean retValue = false;
 		if(resName == null) return retValue;
-		if(resName.equalsIgnoreCase("quicktime")){
+		if(resName.equalsIgnoreCase(QUICKTIME_NAME)){
 			retValue = checkQuickTime();
+		}else if(resName.equalsIgnoreCase(GL4JAVA_NAME)){
+			retValue = checkGL4Java();
 		}
 		return retValue;
 	}
@@ -22,9 +32,26 @@ public final class SystemResourcesChecker
 			Class.forName("quicktime.QTSession");
 			retValue = true;
 		}catch(Throwable t){
-			 retValue = true;
+			 retValue = false;
 		}
 		return retValue;
 	}
 	
+	protected static boolean checkGL4Java(){
+		boolean retValue = false;
+		try{
+		    Class stringClass = String.class;
+			Class clazz = Class.forName("gl4java.GLContext");
+			Method m = clazz.getMethod("loadNativeLibraries",new Class[]{stringClass,stringClass,stringClass});
+            Object retObj = m.invoke(null,new Object[]{null,null,null});
+            if(retObj instanceof Boolean){
+                retValue = ((Boolean)retObj).booleanValue();
+            }
+		}catch(Throwable t){
+			 retValue = false;
+		}
+		return retValue;
+	}
+
 }
+
