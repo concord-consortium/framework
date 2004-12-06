17c752f3e5d4cf70420af3dbd51e34b1ed7af6f9
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2004-12-06 03:23:42 +0000

    otrunk interfaces, these are here so other project can provide objects which can be saved in the otrunk without directly depending on the otrunk.  Currently this set of interfaces only allows "basic" objects to be created.   More complex otrunk objects will want to extend the DefaultOTObject class.
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/core/framework@2169 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/framework/otrunk/OTID.java
@@ -0,0 +1,30 @@
+/*
+ * Last modification information:
+ * $Revision: 1.1 $
+ * $Date: 2004-12-06 03:23:42 $
+ * $Author: scytacki $
+ *
+ * Licence Information
+ * Copyright 2004 The Concord Consortium 
+*/
+package org.concord.framework.otrunk;
+
+
+/**
+ * OTID
+ * This is a mostly opaque object.  However this abstraction
+ * is not currently maintained everywhere.  
+ * 
+ * The toString() method returns a serialized version of this
+ * object.  However to keep the abstraction it would be better 
+ * to remove this, so please try not to use it.
+ *
+ * Date created: Dec 5, 2004
+ *
+ * @author scott<p>
+ *
+ */
+public interface OTID
+{
+
+}
