50275d9a8cb8c8d3a9148723bdeb1373dfe6aa39
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2005-01-11 05:48:42 +0000

    1 class and 2 interfaces moved to framework from OTrunk.  Together these allow more complete use of the OTrunk functionality.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/core/framework@2384 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/framework/otrunk/DefaultOTObject.java
@@ -0,0 +1,80 @@
+/*
+ * Created on Jul 28, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package org.concord.framework.otrunk;
+
+
+
+
+
+/**
+ * This class can be used to create more complex OTObjects.
+ * The OTObject interface can be used for simple interfaces
+ * that adhere to the current conventions of the OTrunk.
+ * If there is an interface that doesn't follow these conventions
+ * you can extend this class and make an OTResourceSchema 
+ * interface to store the state.  The OTrunk will create a 
+ * proxy class that implements this OTResourceSchema.  
+ * 
+ * TODO provide some pointers to examples.
+ * 
+ * @author scott
+ *
+ */
+public class DefaultOTObject implements OTObject
+{
+	private OTrunk otDatabase;
+	
+	private OTResourceSchema resources;
+	
+	public DefaultOTObject(OTResourceSchema resources)
+	{
+		this.resources = resources;		
+	}
+			
+	public OTID getGlobalId()
+	{
+		return resources.getGlobalId();
+	}
+	
+	public String getName()
+	{
+		return resources.getName();
+	}
+	
+	public void setName(String name)
+	{
+		resources.setName(name);
+	}
+		
+	// this needs to be public right now because some views
+	// need the database inorder to handle thirdparty packages
+	// that don't have per instance state.
+	public OTrunk getOTDatabase()
+	{
+		return otDatabase;
+	}
+	
+	public void setOTDatabase(OTrunk otDatabase)
+	{
+		this.otDatabase = otDatabase;
+	}
+	
+	public void init()
+	{
+	}
+	
+	public OTObject getReferencedObject(OTID id)
+	{
+    	try {
+    		OTrunk db = getOTDatabase();
+    		return db.getOTObject(getGlobalId(), id);
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		return null;
+    	}
+	}	
+}
