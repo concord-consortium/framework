ea5c25ca3b0705b0ac0030f606de2a4e33b66499
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2005-01-12 04:16:36 +0000

    2 new resource object types.  These can be used to store collections of basic resources.  They are a lower level version of the object collections.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/core/framework@2402 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/framework/otrunk/OTResourceCollection.java
@@ -0,0 +1,29 @@
+/*
+ * Last modification information:
+ * $Revision: 1.1 $
+ * $Date: 2005-01-12 04:16:36 $
+ * $Author: scytacki $
+ *
+ * Licence Information
+ * Copyright 2004 The Concord Consortium 
+*/
+package org.concord.framework.otrunk;
+
+
+/**
+ * OTResourceCollection
+ * Class name and description
+ *
+ * Date created: Sep 29, 2004
+ *
+ * @author scott<p>
+ *
+ */
+public interface OTResourceCollection
+{
+	public int size();
+
+	public void removeAll();
+
+	
+}
