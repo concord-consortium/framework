867e86f7f136a892ca916aeeeff36f7f09cc8e57
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2005-01-13 03:09:45 +0000

    New interface so the creation of the data producer can be delayed.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/core/framework@2417 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/framework/data/stream/DataProducerProxy.java
@@ -0,0 +1,13 @@
+/*
+ * Created on Jan 12, 2005
+ *
+ */
+package org.concord.framework.data.stream;
+
+/**
+ * @author scytacki
+ */
+public interface DataProducerProxy 
+{
+	public DataProducer getDataProducer();
+}
