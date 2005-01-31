e591ad5eed2ae08b56610533146d21cd9d083ab5
eburke <eburke@6e01202a-0783-4428-890a-84243c50cc2b>
2005-01-31 00:44:27 +0000

    Added NodeState and NodeStateTransition interfaces with corresponding Event objects and Listener interfaces. Used to make Logging and Pedagogica independent of each other at the Java code level.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/core/framework@2590 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/framework/domain/event/NodeStateTransitionEvent.java
@@ -0,0 +1,53 @@
+
+
+/*
+ *  Copyright (C) 2004  The Concord Consortium, Inc.,
+ *  10 Concord Crossing, Concord, MA 01741
+ *
+ *  Web Site: http://www.concord.org
+ *  Email: info@concord.org
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+package org.concord.framework.domain.event;
+
+import java.util.EventObject;
+
+public class NodeStateTransitionEvent
+extends EventObject
+{    
+    /**
+     * Comment for <code>serialVersionUID</code>
+     */
+    private static final long serialVersionUID = 1L;
+    private boolean reverse = false;
+
+    public NodeStateTransitionEvent(Object object)
+    {
+        super(object);
+    }
+
+    public NodeStateTransitionEvent(Object object, boolean reverse)
+    {
+        this(object);
+        this.reverse = reverse;
+    }
+    
+    public boolean isReverse()
+    {
+        return reverse;
+    }
+}
