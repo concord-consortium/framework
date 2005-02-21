565548f48021700208be2e50fbbe28a588a53951
dmarkman <dmarkman@6e01202a-0783-4428-890a-84243c50cc2b>
2005-02-21 06:16:27 +0000

    *** empty log message ***
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/core/framework@3367 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/framework/abouthelper/ApplAboutHandlerFactory.java
@@ -0,0 +1,42 @@
+
+
+/*
+ *  Copyright (C) 2004  The Concord Consortium, Inc.,
+ *  10 Concord Crossing, Concord, MA 01741
+ *
+ *  Web Site: http://www.concord.org
+ *  Email: info@concord.org
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+package org.concord.framework.abouthelper;
+
+public class ApplAboutHandlerFactory
+{
+	public static ApplAboutHandler getApplAboutHandler(){
+		boolean macOS = System.getProperty("os.name").startsWith("Mac OS");
+		if(!macOS) return null;
+		ApplAboutHandler handler = null;
+		try{
+			Class appHandlerClass = Class.forName("org.concord.framework.abouthelper.MacApplAboutHandler");
+			handler =  (ApplAboutHandler)appHandlerClass.newInstance();
+		}catch(Throwable t){
+			handler = null;
+		}
+
+		return handler;
+	}
+}
