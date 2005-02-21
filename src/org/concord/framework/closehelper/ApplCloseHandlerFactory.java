8ecac2c19bd1de0828312e2201e19d5156d3ba64
dmarkman <dmarkman@6e01202a-0783-4428-890a-84243c50cc2b>
2005-02-21 06:09:32 +0000

    *** empty log message ***
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/core/framework@3366 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/framework/closehelper/ApplCloseHandlerFactory.java
+++ src/org/concord/framework/closehelper/ApplCloseHandlerFactory.java
@@ -30,19 +30,9 @@ public class ApplCloseHandlerFactory
 		boolean macOS = System.getProperty("os.name").startsWith("Mac OS");
 		if(!macOS) return null;
 		ApplCloseHandler handler = null;
-		boolean	jdk14 = false;
-		try{
-			Class.forName("java.nio.ByteBuffer");
-			jdk14 = true;
-		}catch(Throwable t){}
 		
 		try{
-			Class appHandlerClass = null;
-			if(jdk14){
-				appHandlerClass = Class.forName("org.concord.client.closehelper.Mac14ApplCloseHandler");
-			}else{
-				appHandlerClass = Class.forName("org.concord.client.closehelper.MacApplCloseHandler");
-			}
+			Class appHandlerClass = Class.forName("org.concord.framework.closehelper.MacApplCloseHandler");
 			handler =  (ApplCloseHandler)appHandlerClass.newInstance();
 		}catch(Throwable t){
 			handler = null;
