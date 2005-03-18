0e1ed1d915d59e034a89d0a65020b6425c1b7fd5
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2005-03-18 05:52:52 +0000

    added change notification interfaces.  these can be used when a view wants to see if its ojbject has changed.
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/core/framework@3612 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/framework/otrunk/OTChangeEvent.java
@@ -0,0 +1,23 @@
+/*
+ * Created on Mar 17, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.concord.framework.otrunk;
+
+import java.util.EventObject;
+
+/**
+ * @author scott
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class OTChangeEvent extends EventObject
+{
+    public OTChangeEvent(Object source)
+    {
+        super(source);
+    }
+}
---
+++ src/org/concord/framework/otrunk/OTChangeNotifying.java
@@ -0,0 +1,20 @@
+/*
+ * Created on Mar 17, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.concord.framework.otrunk;
+
+/**
+ * @author scott
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public interface OTChangeNotifying
+{
+    public void addOTChangeListener(OTChangeListener listener);
+
+    public void removeOTChangeListener(OTChangeListener listener);
+}
