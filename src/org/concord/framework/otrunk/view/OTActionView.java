d20cd14966be022e3e14788890846b1f5e84f080
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2006-05-18 19:48:27 +0000

    Updates to allow creation of OTLoggingRequestView
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/core/framework@6152 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/framework/otrunk/view/OTActionView.java
@@ -0,0 +1,15 @@
+/*
+ * Last modification information:
+ * $Revision: 1.1 $
+ * $Date: 2006-05-18 19:48:27 $
+ * $Author: scytacki $
+ *
+ * Licence Information
+ * Copyright 2004 The Concord Consortium 
+*/
+package org.concord.framework.otrunk.view;
+
+public interface OTActionView
+{
+    public void setAction(OTAction action);
+}
