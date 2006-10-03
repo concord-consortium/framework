cf0ddcf820640974848f48b5af8c445db2d62a9d
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2006-10-03 14:55:01 +0000

    This is a new wrapper class that can automatically do the load and save methods based on the methods in the wrapper and the methods in the real object.  These methods are cached.  This class still needs work, before I'd recommend others to use it.  And the caching could be improved since it only needs to be done once for each pair of classes.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/core/framework@6585 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/framework/otrunk/ReflectiveOTWrapper.java
@@ -0,0 +1,180 @@
+package org.concord.framework.otrunk;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Vector;
+
+public class ReflectiveOTWrapper extends DefaultOTWrapper 
+{
+	private OTResourceSchema resources;
+	
+	protected Vector loadMappings;
+	protected Vector saveMappings;
+	
+	public ReflectiveOTWrapper(OTResourceSchema resources) {
+		super(resources);
+		this.resources = resources;
+	}	
+	
+	public void loadRealObject(OTWrapperService wrapperService,
+			Object realObject) {
+		// Go through all the methods of the resources class
+		// and see if there is a matching method in the realObject
+		// class.  Then call that method
+		// look for getProperty
+		// then look for setProperty
+		Class resourcesClass = resources.getClass();
+		Method[] methods = resourcesClass.getMethods();
+		
+		Class realObjClass = realObject.getClass();
+
+		if(loadMappings == null){
+			loadMappings = new Vector();
+
+			for(int i=0; i<methods.length; i++){
+				Method method = methods[i];
+				String name = method.getName();
+				if(method.getParameterTypes().length != 0){
+					continue;
+				}
+				
+				String propertyPart = null;
+				if(name.startsWith("get")){
+					propertyPart = name.substring(3);
+				} else if(name.startsWith("is")){
+					propertyPart = name.substring(2);
+				} else {
+					continue;
+				}
+				String propertyName = propertyPart.substring(0,1).toLowerCase() + propertyPart.substring(1);
+				Class propertyClass = method.getReturnType();
+
+				Method realObjMethod = null;
+				try {
+					realObjMethod = realObjClass.getMethod("set" + propertyPart, new Class[]{propertyClass});
+				} catch (SecurityException e) {
+					e.printStackTrace();
+					continue;
+				} catch (NoSuchMethodException e) {
+					continue;
+				}
+
+				Mapping mapping = new Mapping();
+				mapping.propertyName = propertyName;
+				mapping.getMethod = method;
+				mapping.setMethod = realObjMethod;
+
+				loadMappings.add(mapping);
+
+			}
+		} 
+		
+		
+		for(int i=0; i<loadMappings.size(); i++){
+			Mapping mapping = (Mapping)loadMappings.get(i);
+			try {
+				if(resources.isResourceSet(mapping.propertyName)){
+					Object value = mapping.getMethod.invoke(resources, null);
+					mapping.setMethod.invoke(realObject, new Object[]{value});
+				}
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+		}
+	}
+
+	public void registerRealObject(OTWrapperService wrapperService,
+			Object realObject) {
+	}
+
+	public void saveRealObject(OTWrapperService wrapperService,
+			Object realObject) {
+		// Go through all the methods of the resources class
+		// and see if there is a matching method in the realObject
+		// class.  Then call that method
+		// look for getProperty
+		// then look for setProperty
+		Class resourcesClass = resources.getClass();
+		Method[] methods = resourcesClass.getMethods();
+		
+		Class realObjClass = realObject.getClass();
+
+		if(saveMappings == null){
+			saveMappings = new Vector();
+
+			for(int i=0; i<methods.length; i++){
+				Method resourceMethod = methods[i];
+				String name = resourceMethod.getName();
+				if(!name.startsWith("set") ||
+						resourceMethod.getParameterTypes().length != 1){
+					continue;
+				}				
+				String propertyPart = name.substring(3);
+				String propertyName = propertyPart.substring(0,1).toLowerCase() +
+				propertyPart.substring(1);
+				Class propertyClass = resourceMethod.getParameterTypes()[0];
+
+				Method realObjMethod = null;
+				try {
+					realObjMethod = realObjClass.getMethod("get" + propertyPart, null);
+				} catch (SecurityException e) {
+					e.printStackTrace();
+					continue;
+				} catch (NoSuchMethodException e) {
+					// look for the is method
+					try {
+						realObjMethod = realObjClass.getMethod("is" + propertyPart, null);
+					} catch (SecurityException ex) {
+						e.printStackTrace();
+						continue;
+					} catch (NoSuchMethodException ex) {
+						continue;
+					}
+				}
+
+				if(realObjMethod.getReturnType() != propertyClass){
+					// not quite a match
+					continue;
+				}
+
+				Mapping mapping = new Mapping();
+				mapping.propertyName = propertyName;
+				mapping.getMethod = realObjMethod;
+				mapping.setMethod = resourceMethod;
+
+				saveMappings.add(mapping);
+			}
+		}
+		
+		for(int i=0; i<saveMappings.size(); i++){
+			Mapping mapping = (Mapping)saveMappings.get(i);
+			try {
+				Object value = mapping.getMethod.invoke(realObject, null);
+				mapping.setMethod.invoke(resources, new Object[]{value});
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+		}
+	}
+
+	public static class Mapping
+	{
+		String propertyName;
+		Method getMethod;
+		Method setMethod;
+	}
+}
