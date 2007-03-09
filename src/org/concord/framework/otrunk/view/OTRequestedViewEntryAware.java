fbf114f291409e766c1ad2bc2323d2fa1abbff68
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2007-03-09 11:46:28 +0000

    Added ability to access the view entry which was used in the request which resulted in this a view being created.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/core/framework@6990 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/framework/otrunk/view/OTRequestedViewEntryAware.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package org.concord.framework.otrunk.view;
+
+/**
+ * @author scott
+ *
+ */
+public interface OTRequestedViewEntryAware extends OTView 
+{
+	public void setRequestedViewEntry(OTViewEntry viewEntry);
+}
