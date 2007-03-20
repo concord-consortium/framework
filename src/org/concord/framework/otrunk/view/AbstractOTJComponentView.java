52a497489ec56b9819d002b8791622661b135456
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2007-03-20 21:18:29 +0000

    Added another convience method to AbstractOTJComponentView. Fixed the handling of object without views.  And cleand up the document view a little.
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/core/framework@7074 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/framework/otrunk/view/AbstractOTJComponentView.java
+++ src/org/concord/framework/otrunk/view/AbstractOTJComponentView.java
@@ -15,11 +15,15 @@ public abstract class AbstractOTJComponentView
 	extends AbstractOTView
 implements OTJComponentView 
 {
+	public OTJComponentService getJComponentService()
+	{
+		return (OTJComponentService)getViewService(OTJComponentService.class);		
+	}
+	
 	public JComponent getChildComponent(OTObject otObject,
 			OTViewContainer container, boolean editable)
 	{
-		OTJComponentService jComponentService = 
-			(OTJComponentService)getViewService(OTJComponentService.class);
+		OTJComponentService jComponentService = getJComponentService();
 		return jComponentService.getComponent(otObject, container, editable);
 	}
 }
