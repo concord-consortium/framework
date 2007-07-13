abff5e70e7c8840ac8ae10789ae94cff3c98db80
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2007-07-13 16:00:34 +0000

    added x, y attributes to OTFrame
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/core/framework@7873 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/framework/otrunk/view/OTMainComponentProvider.java
+++ src/org/concord/framework/otrunk/view/OTMainComponentProvider.java
@@ -2,6 +2,15 @@ package org.concord.framework.otrunk.view;
 
 import java.awt.Component;
 
+/**
+ * This interface extends OTDefaultComponentProvider to allow authorability of
+ * the snapshots. getMainComponent should return the top-level component in the
+ * hierarchy, and getAllComponents should return an array of all snapshotable
+ * components
+ * 
+ * @author sfentress
+ *
+ */
 public interface OTMainComponentProvider extends OTDefaultComponentProvider {
 	public Component getMainComponent();
 	public Component[] getAllComponents();
