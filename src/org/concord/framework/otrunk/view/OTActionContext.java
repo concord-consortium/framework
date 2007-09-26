d9fa472e632716138c456153796aa173286394d2
imoncada <imoncada@6e01202a-0783-4428-890a-84243c50cc2b>
2007-09-26 18:39:32 +0000

    Added OTFrameDisplayAction, which is an action that shows an otobject inside of a specific frame. I had to add a new OTActionContext interface so the action can have access to the view context from the class calling doAction(). All the classes implementing OTAction were modified accordingly.
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/core/framework@8313 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/framework/otrunk/view/OTActionContext.java
@@ -0,0 +1,25 @@
+/*
+ * Last modification information:
+ * $Revision: 1.1 $
+ * $Date: 2007-09-26 18:39:32 $
+ * $Author: imoncada $
+ *
+ * Licence Information
+ * Copyright 2007 The Concord Consortium 
+*/
+package org.concord.framework.otrunk.view;
+
+
+/**
+ * OTActionContext
+ * Class name and description
+ *
+ * Date created: Sep 26, 2007
+ *
+ * @author Ingrid Moncada<p>
+ *
+ */
+public interface OTActionContext
+{
+	public OTViewContext getViewContext();
+}
