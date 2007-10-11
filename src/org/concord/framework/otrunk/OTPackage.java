bac87d6929b6687f29a7c60aa1ddfc2235aa5cf5
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2007-10-11 20:15:06 +0000

    Add support for dependent OTPackages, and changed the OTPackage API.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/core/framework@8426 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/framework/otrunk/OTPackage.java
+++ src/org/concord/framework/otrunk/OTPackage.java
@@ -3,7 +3,6 @@
  */
 package org.concord.framework.otrunk;
 
-import java.util.List;
 
 /**
  * @author scott
@@ -11,7 +10,18 @@ import java.util.List;
  */
 public interface OTPackage
 {
+	/**
+	 * This should return a list of class which implement OTPackage.
+	 * This method should be called before initialize.  However if there are circular
+	 * dependencies then the order they are initialized is undetermined.
+	 * 
+	 * These packages will be initialized before this package is initialized.
+	 * 
+	 * @return null if there are no dependencies.
+	 */
+	public Class [] getPackageDependencies();
+	
 	public void initialize(OTrunk otrunk);
 	
-	public List getOTClasses();
+	public Class [] getOTClasses();
 }
