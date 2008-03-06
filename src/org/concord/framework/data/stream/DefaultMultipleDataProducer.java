264dea11920b72c8e6258c3f59da431081593ad3
qxie <qxie@6e01202a-0783-4428-890a-84243c50cc2b>
2008-03-06 20:17:51 +0000

    Add clearAll to allow DataGraph to remove all the DataGraphables
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/core/framework@12793 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/framework/data/stream/DefaultMultipleDataProducer.java
+++ src/org/concord/framework/data/stream/DefaultMultipleDataProducer.java
@@ -29,27 +29,25 @@
  *
  * Licence Information
  * Copyright 2004 The Concord Consortium 
-*/
+ */
 package org.concord.framework.data.stream;
 
-
 /**
- * DefaultMultipleDataProducer
- * Class name and description
- *
+ * DefaultMultipleDataProducer Class name and description
+ * 
  * Date created: Sep 9, 2004
- *
- * @author imoncada<p>
- *
+ * 
+ * @author imoncada
+ *         <p>
+ * 
  */
-public class DefaultMultipleDataProducer extends DefaultDataProducer
-{
+public class DefaultMultipleDataProducer extends DefaultDataProducer {
 	boolean valuesSent = true;
+
 	/**
 	 * 
 	 */
-	public DefaultMultipleDataProducer()
-	{
+	public DefaultMultipleDataProducer() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
@@ -57,104 +55,109 @@ public class DefaultMultipleDataProducer extends DefaultDataProducer
 	/**
 	 * @param size
 	 */
-	public DefaultMultipleDataProducer(int size)
-	{
+	public DefaultMultipleDataProducer(int size) {
 		this(1, size);
 	}
-	
+
 	/**
-	 * This method is deprecated because it creates a data producer
-	 * that has only one channels. If there is only one 
-	 * channel then just use the DefaultDataProducer.
+	 * This method is deprecated because it creates a data producer that has
+	 * only one channels. If there is only one channel then just use the
+	 * DefaultDataProducer.
 	 * 
-	 * previous this method was used when the number of channels
-	 * was unknown.  then when addValues was called the number of 
-	 * channels was dynamically updated.  This caused irradic behavior
-	 * in, so now the number of channels must be set explicitly.
+	 * previous this method was used when the number of channels was unknown.
+	 * then when addValues was called the number of channels was dynamically
+	 * updated. This caused irradic behavior in, so now the number of channels
+	 * must be set explicitly.
 	 * 
 	 * @deprecated
 	 * @param dt
 	 */
-	public DefaultMultipleDataProducer(float dt)
-	{
+	public DefaultMultipleDataProducer(float dt) {
 		super(dt);
 	}
-	
+
 	/**
 	 * @param dt
 	 */
-	public DefaultMultipleDataProducer(float dt, int size)
-	{
+	public DefaultMultipleDataProducer(float dt, int size) {
 		super(dt);
 		getDataDescription().setChannelsPerSample(size);
 	}
 
-	public void addValues(float[] vals)
-	{
+	public void addValues(float[] vals) {
 		addValues(vals, true);
 	}
-	
+
 	/**
-	 * The size of the vals array must match the number of channels.
-	 * if it doesn't an ArrayStoreException will be thrown.
+	 * The size of the vals array must match the number of channels. if it
+	 * doesn't an ArrayStoreException will be thrown.
 	 * 
 	 * The number of channels can be changed after initialization using
 	 * getDataDescription().setChannelsPerSample(size)
 	 * 
-	 * if bSendValues is true then the data will be sent on to 
-	 * data consumers listening to this producer.  Otherwise the 
-	 * data will accumulate. 
+	 * if bSendValues is true then the data will be sent on to data consumers
+	 * listening to this producer. Otherwise the data will accumulate.
 	 * 
 	 * @param vals
 	 * @param bSendValues
 	 */
-	public void addValues(float[] vals, boolean bSendValues)
-	{
-		if(vals.length != getDataDescription().getChannelsPerSample()) {
-			throw new ArrayStoreException("Size of array (" + vals.length + ") " +
-					"doesn't match number of channels (" + getDataDescription().getChannelsPerSample() + ")");
+	public void addValues(float[] vals, boolean bSendValues) {
+		if (vals.length != getDataDescription().getChannelsPerSample()) {
+			throw new ArrayStoreException("Size of array (" + vals.length
+					+ ") " + "doesn't match number of channels ("
+					+ getDataDescription().getChannelsPerSample() + ")");
 		}
-		
+
 		float[] tempValues;
-		if (!valuesSent){
+		if (!valuesSent) {
 			tempValues = new float[vals.length + this.values.length];
-			for (int i=0; i<tempValues.length; i++){
-				if (i < this.values.length){
+			for (int i = 0; i < tempValues.length; i++) {
+				if (i < this.values.length) {
 					tempValues[i] = this.values[i];
-				}
-				else{
+				} else {
 					tempValues[i] = vals[i - this.values.length];
 				}
 			}
-			
-			dataEvent.setNumSamples(dataEvent.getNumSamples()+1);
-		}
-		else if (!bSendValues){
+
+			dataEvent.setNumSamples(dataEvent.getNumSamples() + 1);
+		} else if (!bSendValues) {
 			tempValues = new float[vals.length];
-			for (int i=0; i<tempValues.length; i++){
+			for (int i = 0; i < tempValues.length; i++) {
 				tempValues[i] = vals[i];
 			}
-			
+
 			dataEvent.setNumSamples(1);
-		}
-		else{
+		} else {
 			tempValues = vals;
 		}
 		this.values = tempValues;
-		
+
 		dataEvent.setData(this.values);
-		
-		if (dataDesc.getChannelsPerSample() != vals.length){
+
+		if (dataDesc.getChannelsPerSample() != vals.length) {
 			dataDesc.setChannelsPerSample(vals.length);
 			notifyDataStreamEvent(DataEvent.DATA_DESC_CHANGED);
 		}
-		
-		if (bSendValues){
+
+		if (bSendValues) {
 			notifyDataReceived();
 			valuesSent = true;
-		}
-		else{
-			valuesSent = false;		
+		} else {
+			valuesSent = false;
 		}
 	}
+
+	// XIE: this flag specifies whether we should remove all the graphs that
+	// come from the same DataProducer when we press the reset() method is
+	// called.
+	private boolean clearAll;
+
+	public void setClearAll(boolean b) {
+		clearAll = b;
+	}
+
+	public boolean getClearAll() {
+		return clearAll;
+	}
+
 }
