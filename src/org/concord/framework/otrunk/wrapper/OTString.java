11a41516d1c23bf97c5b4c46662d6c4b694a890a
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2008-08-01 14:16:29 +0000

    Moved OTObjectSet here Added OTString wrapper.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/core/framework@14606 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/framework/otrunk/wrapper/OTString.java
@@ -0,0 +1,9 @@
+package org.concord.framework.otrunk.wrapper;
+
+import org.concord.framework.otrunk.OTObjectInterface;
+
+public interface OTString extends OTObjectInterface
+{
+	public String getString();
+	public void setString(String string);
+}
