146874ce26243716d0990fb9743cf772d2aa2e5b
aunger <aunger@6e01202a-0783-4428-890a-84243c50cc2b>
2009-04-07 16:31:48 +0000

    Add a wrapper for OTObjectMap.
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/core/framework@18003 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/org/concord/framework/otrunk/wrapper/OTHash.java
@@ -0,0 +1,8 @@
+package org.concord.framework.otrunk.wrapper;
+
+import org.concord.framework.otrunk.OTObjectInterface;
+import org.concord.framework.otrunk.OTObjectMap;
+
+public interface OTHash extends OTObjectInterface {
+	public OTObjectMap getEntries();
+}
