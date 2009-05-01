37be6066b2fd5cfa47806f63d6000b381594be50
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2009-05-01 15:22:48 +0000

    Updated the ot multi data graph view to use the startable toolbar. Updated the startable api and implementation to correctly handle customized strings for the startable actions.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/core/framework@18527 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/framework/startable/StartableInfo.java
+++ src/org/concord/framework/startable/StartableInfo.java
@@ -2,11 +2,39 @@ package org.concord.framework.startable;
 
 public class StartableInfo {
 	public boolean enabled = true;
+	
+	/**
+	 * If this is false (the default), then buttons for controlling the startable should 
+	 * always be enabled since they won't know if the startable changed it outside of the 
+	 * buttons.
+	 */
+	public boolean sendsEvents;
 	public boolean canResetWhileRunning = false;
 	public boolean canRestartWithoutReset = false;
-	public String label;
+	
 	public String startVerb;
 	public String stopVerb;
 	public String resetVerb;
 	public String startStopVerb;
+
+	/**
+	 * This will be appended after the start, stop verb and a space. 
+	 * So if the label is "X" then the action description will be "Start X"
+	 */
+	public String startStopLabel;
+
+	
+	/**
+	 * This will be appended after the reset verb and a space. 
+	 * So if the label is "X" then the action description will be "Reset X"
+	 */
+	public String resetLabel;
+
+	public static final String DEFAULT_START_VERB = "Start";
+
+	public static final String DEFAULT_STOP_VERB = "Stop";
+
+	public static final String DEFAULT_RESET_VERB = "Reset";
+
+	public static final String DEFAULT_START_STOP_VERB = "Start/Stop";
 }
