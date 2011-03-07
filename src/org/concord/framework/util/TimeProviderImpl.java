8726957f6c6eb74d1837e00c47a17d6926e412b0
aunger <aunger@6e01202a-0783-4428-890a-84243c50cc2b>
2011-03-07 21:23:14 +0000

    Informational logging message.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/core/framework@24347 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/framework/util/TimeProviderImpl.java
+++ src/org/concord/framework/util/TimeProviderImpl.java
@@ -1,11 +1,18 @@
 package org.concord.framework.util;
 
+import java.util.logging.Logger;
+
 /**
  * Standard implementation of the TimeProvider interface. Simply wraps the system clock.
  * @author aunger
  *
  */
 public class TimeProviderImpl implements TimeProvider {
+    private static final Logger logger = Logger.getLogger(TimeProviderImpl.class.getName());
+    
+    public TimeProviderImpl() {
+        logger.info("Creating default TimeProvider...");
+    }
 
     public long currentTimeMillis() {
         return System.currentTimeMillis();
